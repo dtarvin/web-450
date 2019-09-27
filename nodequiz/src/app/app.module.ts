75b31c8c3d55ba8e970ca7e975dd37eb40f9108a
David R Tarvin Jr <david.tarvin@gmail.com>
2019-09-27 06:20:26 -0500

    create login form

--- nodequiz/src/app/app.module.ts
+++ nodequiz/src/app/app.module.ts
@@ -12,22 +12,59 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutes } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './shared';
+import { CumulativeSummaryComponent } from './pages/cumulative-summary/cumulative-summary.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+
+import { AuthGuard } from './shared/guards/auth.guard';
+import { CookieService } from 'ngx-cookie-service';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BaseLayoutComponent
+    BaseLayoutComponent,
+    CumulativeSummaryComponent,
+    DashboardComponent,
+    LoginComponent,
+    NotFoundComponent,
+    AuthLayoutComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(AppRoutes, { useHash: true, enableTracing: false }),
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},
+              AuthGuard, CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
