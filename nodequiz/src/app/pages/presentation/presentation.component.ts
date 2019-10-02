142ed9f21284ba7fa279179748cc8c5b369f5ab2
a161120 <david.tarvin@physiciansmutual.com>
2019-10-02 15:29:00 -0500

    commit

--- nodequiz/src/app/pages/presentation/presentation.component.ts
+++ nodequiz/src/app/pages/presentation/presentation.component.ts
@@ -2,6 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PresentationService } from './presentation.service';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+export interface Presentation {
+  name: string;
+  image: string[];
+}
 
 @Component({
   selector: 'app-presentation',
@@ -9,19 +16,54 @@ import { PresentationService } from './presentation.service';
   styleUrls: ['./presentation.component.css']
 })
 export class PresentationComponent implements OnInit {
-
+  // presentation;
   presentationName: string;
-  slides: any[];
+  // slides: any[];
+  presentation: any;
+  rest: any;
+  valueStream: any;
+  waterfall: any;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient,
-              private presentationService: PresentationService) {
-    this.presentationName = route.snapshot.paramMap.get('name');
-    this.presentationService.getPresentations()
-        .subscribe(data => {
+  // constructor(private route: ActivatedRoute, private http: HttpClient,
+  //             private presentationService: PresentationService) {
+  //   this.presentationName = route.snapshot.paramMap.get('name');
+  //   this.presentation = this.presentationService.getPresentation(this.presentationName);
+  //       .filter(p => p.name === this.presentationName);
+        
+  // }  
 
-        })
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+    
+    this.presentationName = route.snapshot.paramMap.get('name'); 
+    if (this.presentationName = 'rest-and-the-web') {
+      this.presentation = 'rest';
+    } else if (this.presentationName = 'technology-value-stream') {
+      this.presentation = 'valueStream';
+    } else {
+      this.presentation = 'waterfall';
+    }
+
+    this.rest = [
+      { name: "Slide1"}, { name: "Slide2"}, { name: "Slide3"}, { name: "Slide4"},
+      { name: "Slide1"}, { name: "Slide6"}, { name: "Slide7"}, { name: "Slide8"},
+      { name: "Slide9"}, { name: "Slide10"}, { name: "Slide11"}, { name: "Slide12"},
+      { name: "Slide13"}, { name: "Slide14"}, { name: "Slide15"}
+    ];
+    this.valueStream = [
+      { name: "Slide1"}, { name: "Slide2"}, { name: "Slide3"}, { name: "Slide4"},
+      { name: "Slide1"}, { name: "Slide6"}, { name: "Slide7"}, { name: "Slide8"},
+      { name: "Slide9"}, { name: "Slide10"}, { name: "Slide11"}, { name: "Slide12"},
+      { name: "Slide13"}
+    ];
+    this.waterfall = [
+      { name: "Slide1"}, { name: "Slide2"}, { name: "Slide3"}, { name: "Slide4"},
+      { name: "Slide1"}, { name: "Slide6"}, { name: "Slide7"}, { name: "Slide8"},
+      { name: "Slide9"}, { name: "Slide10"}, { name: "Slide11"}, { name: "Slide12"},
+      { name: "Slide13"}, { name: "Slide14"}, { name: "Slide15"}, { name: "Slide16"},
+      { name: "Slide17"}, { name: "Slide18"}, { name: "Slide19"}, { name: "Slide20"},
+      { name: "Slide21"}, { name: "Slide22"}, { name: "Slide23"}
+    ];
   }
-
 
   ngOnInit() {
   }
--- nodequiz/src/app/pages/presentation/presentation.service.ts
+++ nodequiz/src/app/pages/presentation/presentation.service.ts
@@ -1,14 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+export interface Presentation {
+  name: string;
+  image: string[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PresentationService {
 
-  constructor(private http: HttpClient) { }
+  // presentation: Presentation[];
+  presentationName: string;
+  presentations;
 
-  getPresentations() {
-    return this.http.get('./assets/presentations.json');
+  constructor(private http: HttpClient) { 
+    // http.get('./assets/presentations')
+    // .pipe(map(data => {}))
+    // .subscribe(data => this.presentations = data);
   }
+  
+  // getPresentation(presentationName) {
+  //   const presentation = this.presentations.filter(p => p.name === this.presentationName);
+  //   return presentation.image;
+  // }
+
+  getPresentation(presentationName) {
+    this.http.get('./assets/presentations.json')
+    .subscribe(res => {
+      this.presentations = res;
+      this.presentations.filter(p => this.presentations.name === this.presentationName);  
+      return this.presentations.image.name;
+    })   
+  }
+
+  // getPresentation(presentationName: string): Observable<Presentation[]> {
+  //   return this.http.get<Presentation[]>('./assets/presentations.json').pipe(
+  //     map(this.presentations.filter(p => p.name === presentationName)));
+  // }
+
+  // getPresentation(presentationName): Observable<any> {
+  //   return this.http.get('./assets/presentations')
+  //     .pipe(filter(p => p.name === this.presentationName);
+
+  // }
 }
